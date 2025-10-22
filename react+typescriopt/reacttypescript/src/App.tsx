27e49d926a4543df43535b3408b8cef10ea85149
Bhargavashokdollu <bhargavd@aapmor.com>
2025-10-22 18:26:00 +0530

    Event Props

--- react+typescriopt/reacttypescript/src/App.tsx
+++ react+typescriopt/reacttypescript/src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import Props from './components/Props'
+import Button from './components/Button'
+import Input from './components/Input'
 function App() {
   
 
@@ -11,7 +13,14 @@ function App() {
       <Props name='Ashok' msg={10}/>
       <Props name='kumar' msg={20}/> */}
 <Props name='Ashok' age={30} salary={10000}  isActive={true} department="development" position='Frontend Developer'/>
+<Button handleClick={(event,id)=>{
+  console.log('Button Clicked',event,id)
+}
+
+} />
+<Input value='' handleChange={(event => console.log(event))}/>
     </>
   )
+  
 }
 export default App
